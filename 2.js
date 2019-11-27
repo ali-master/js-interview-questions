d8682c733378ab05d982bd3c3d2783a13a8dc788
mssoheil88 <mssoheil88@yahoo.com>
2019-11-27 09:28:31 +0330

    fix: corrected the answers

--- 2.js
+++ 2.js
@@ -1,19 +1,19 @@
 //What's the output?
 
 for (var i = 0; i < 4; i++) {
-	setTimeout(() => {
-		console.log(i);
-	}, 1);
+  setTimeout(() => {
+    console.log(i);
+  }, 1);
 }
 
 for (let i = 0; i < 4; i++) {
-	setTimeout(() => {
-		console.log(i);
-	}, 1);
+  setTimeout(() => {
+    console.log(i);
+  }, 1);
 }
 
 /*
-	A: 0 1 2 and 0 1 2
-	B: 0 1 2 and 3 3 3
-	C: 3 3 3 and 0 1 2
+	A: 0 1 2 3 and 0 1 2 3
+	B: 0 1 2 3 and 3 3 3 3
+	C: 4 4 4 4 and 0 1 2 3
 */
